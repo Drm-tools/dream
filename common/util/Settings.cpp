a9ae7e37470267eaebc0c4c6f31711ec28952f7a
Andrea Russo <andrusso@yahoo.com>
2006-11-27 18:52:58 +0000

    Re-insert the possibility of set latitude and longitude from command line.

--- common/util/Settings.cpp
+++ common/util/Settings.cpp
@@ -1039,6 +1039,23 @@ _BOOLEAN CSettings::ParseArguments(int argc, char** argv)
 			continue;
 		}
 
+		/* Latitude string for log file ------------------------------------- */
+		if (GetStringArgument(argc, argv, i, "-a", "--latitude",
+			strArgument) == TRUE)
+		{
+			pDRMRec->GetParameters()->ReceptLog.SetLatitude(strArgument);
+			continue;
+		}
+
+
+		/* Longitude string for log file ------------------------------------ */
+		if (GetStringArgument(argc, argv, i, "-o", "--longitude",
+			strArgument) == TRUE)
+		{
+			pDRMRec->GetParameters()->ReceptLog.SetLongitude(strArgument);
+			continue;
+		}
+
 
 		/* Color scheme main plot ------------------------------------------- */
 		if (GetNumericArgument(argc, argv, i, "-y", "--colorscheme", 0,
