f41c8667e535d3ee250780bf4c68b9e6d8762502
jcable <julian.cable@yahoo.com>
2010-04-18 16:45:37 +0100

    make sure QDom field's constructor is called

--- common/datadecoding/epg/epgdec.h
+++ common/datadecoding/epg/epgdec.h
@@ -13,16 +13,16 @@
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later 
+ * Foundation; either version 2 of the License, or (at your option) any later
  * version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  *
  * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 
+ * this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
 \******************************************************************************/
@@ -37,9 +37,9 @@ using namespace std;
 class tag_length_value
 {
 public:
-       
+
       tag_length_value(const _BYTE* p);
-      
+
       bool is_cdata() const { return tag == 1; }
       bool is_epg() const { return tag == 2; }
       bool is_service_information() const { return tag == 3; }
@@ -56,7 +56,7 @@ public:
 class CEPGDecoder
 {
   public:
-    CEPGDecoder ()
+    CEPGDecoder ():doc()
     {
     }
     void decode (const vector<_BYTE>&);
