9335667967f68350e7fa2251b6ff3f9112a19c4e
Julian Cable <julian.cable@yahoo.com>
2025-12-19 10:35:23 +0000

    Update Sound.cpp

--- src/windows/Sound.cpp
+++ src/windows/Sound.cpp
@@ -105,7 +105,7 @@ CSoundInMMSystem::~CSoundInMMSystem()
         CloseHandle(m_WaveEvent);
 }
 
-bool CSoundInMMSystem::Read(CVector<short>& psData)
+bool CSoundInMMSystem::Read(CVector<short>& psData, CParameter&)
 {
     int			i;
     bool	bError;
@@ -683,4 +683,5 @@ void CSoundOutMMSystem::Close()
     /* Set flag to open devices the next time it is initialized */
     bChangDev = true;
 }
+
 
