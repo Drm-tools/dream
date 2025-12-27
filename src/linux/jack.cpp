baa35e690e26c45882726fdba62fb22c4b0e6f17
Julian Cable <julian.cable@yahoo.com>
2025-12-27 23:54:40 +0000

    wip

--- src/linux/jack.cpp
+++ src/linux/jack.cpp
@@ -49,7 +49,9 @@ void CJackSoundIn::Enumerate(std::vector<std::string>& names, std::vector<std::s
             // Try to get port alias as friendly name
             jack_port_t* port = jack_port_by_name(temp_client, ports[i]);
             if (port) {
-                const char aliases[2][jack_port_name_size()];
+                char* aliases[2];
+                aliases[0] = new char[jack_port_name_size()];
+			    aliases[1] = new char[jack_port_name_size()];
                 int num_aliases = jack_port_get_aliases(port, aliases);
                 if (num_aliases > 0) {
                     descriptions.push_back(aliases[0]);
