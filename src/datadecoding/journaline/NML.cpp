a51e8f663e02538f134f53eef42970a7a83f9911
Julian Cable <julian.cable@yahoo.com>
2025-12-29 10:56:18 +0000

    Update NML.cpp
    c++17

--- src/datadecoding/journaline/NML.cpp
+++ src/datadecoding/journaline/NML.cpp
@@ -53,7 +53,7 @@
 /// handling of NML (news markup language for Journaline) data
 ///
 /// @file       NML.cpp
-/// @author     Michael Reichenb‰cher <rbr@iis.fraunhofer.de>
+/// @author     Michael Reichenb√§cher <rbr@iis.fraunhofer.de>
 ///
 /// $Id: NML.cpp,v 1.9 2012/09/30 10:33:58 jcable Exp $
 ///
@@ -264,9 +264,9 @@ std::string NML::Dump(void) const
 //returns 0 on error
 unsigned char* NMLFactory::getNextSection( const unsigned char*& p, unsigned short& plen, unsigned short& reslen )
 {
-	register int i = 0;
-	register int j = 0;
-	register int dslen;
+	int i = 0;
+	int j = 0;
+	int dslen;
 	unsigned char* res = (unsigned char*)malloc( plen + 1 );
 
 	// we will loop through the entire data until a break case is met
