b64a3e26bc27470933e25d5e96354b6992964f56
Peter Adam Homolya <hpeter3@disroot.org>
2026-03-19 18:05:05 +0100

    Fixing bugs/compile errors related to gpsd, transmitter GUI and CMake (#46)
    Fixing:
    
    hpeter3/dream#8
    hpeter3/dream#7 (partial fix, I'll look into "1. Input defaults to OSS internally, causing no audio capture", I couldn't reproduce it yet)
    gpsd (upstream deprecated gps_data.status)
    Hamlib (small upstream change)
    
    CMake:
    
    Redid the CMake file, this isn't 100% complete yet, but it works better than before.
    Created audiobackends.cmake and hardening.cmake so far, I'll probably do an audiocodecs.cmake and a consolemode.cmake in the near future.
    hardening.cmake:
    
    It's enabled by default but can be disabled via -DENABLE_HARDENING=OFF (for more CMake options see: CMakeLists.txt)
    
    Misc:
    
    there are some mismatches from commits (I messed up my git stuff before, some commits are just fixes for that)
    These are:
    f9721b3
    aa4414b
    14de2a4
    c68a83b

--- src/sourcedecoders/fdk_aac_codec.cpp
+++ src/sourcedecoders/fdk_aac_codec.cpp
@@ -25,7 +25,7 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
 \******************************************************************************/
-//DEBUG2025
+
 #include "fdk_aac_codec.h"
 #include <fdk-aac/aacenc_lib.h>
 #include <fdk-aac/FDK_audio.h>
@@ -707,3 +707,4 @@ FdkAacCodec::fileName(const CParameter& Parameters) const
 
     return ss.str();
 }
+
